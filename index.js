31bec163449e0f651f5efa43b8b6a6a938212f17
Joseph <ojppresido@gmail.com>
2021-02-24 01:11:22 +0800

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-
+const cors = require('cors');
 const compression = require('compression');
 const helmet = require('helmet');
 const winston = require('winston');
-// const error =  require('./middleware/error');
+const error =  require('./middleware/error');
 const mongoose = require('mongoose');
 const staffs = require('./route/staffs');
 const authent = require('./route/authent');
@@ -18,11 +18,12 @@ mongoose.connect('mongodb://localhost/sagSTAFF')
 .catch(err=>console.error('Could not connect', err))
 
 
+app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.use('/staff', staffs);
 app.use('/authent', authent);
-// app.use(error);
+app.use(error);
 
 
 
